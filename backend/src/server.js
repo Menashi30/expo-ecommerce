63919ae27343ff0a95d3a0e8e4da2985ce28f249
Menashi30 <menashi30@gmail.com>
2026-01-09 22:40:20 -0500

    server file config for express asset

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,11 +13,11 @@ app.get("/api/health", (req, res) => {
 //make the app ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../admin/dist")));
-}
 
-app.get("/{*any}", (req, res) => {
-  res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
-});
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
+}
 
 app.listen(ENV.PORT, () => {
   console.log("server is up and running");
