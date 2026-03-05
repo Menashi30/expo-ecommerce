f0bb562ae08f8ac5197436b224d20035e8bb6767
Menashi30 <menashi30@gmail.com>
2026-03-05 00:18:17 -0500

    Revert "admin routes changes"

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,8 +7,6 @@ import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./config/inngest.js";
 
-import adminRoutes from "./routes/admin.route.js";
-
 const app = express();
 
 const __dirname = path.resolve();
@@ -18,14 +16,13 @@ app.use(express.json());
 app.use(clerkMiddleware()); // adds auth object under the req => req.auth
 // Set up the "/api/inngest" (recommended) routes with the serve handler
 app.use("/api/inngest", serve({ client: inngest, functions }));
-app.use("/api/admin", adminRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "success" });
 });
 
 //make the app ready for deployment
-if (ENV.NODE_ENV === "development") {
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../admin/dist")));
 
   app.get("/{*any}", (req, res) => {
